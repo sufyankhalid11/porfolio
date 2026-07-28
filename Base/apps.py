b88f85afe909cf06d86c450a9ea176a49f3bdc02
Muhammad Sufyan Khalid <msufyankhalid1@example.com>
2026-07-28 13:55:04 +0500

    Auto-run migrations on app startup for Vercel

--- Base/apps.py
+++ Base/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
-
+from django.db.utils import OperationalError, ProgrammingError
 
 class BaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'Base'
+
+    def ready(self):
+        # Yeh code automatically startup par tables migrate kar dega agar missing huiin
+        try:
+            from django.core.management import call_command
+            call_command('migrate', interactive=False)
+        except Exception:
+            pass    
